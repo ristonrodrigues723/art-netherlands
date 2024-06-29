0cdedc300d9ff40337b86c69150d0a5a27fc2bb7
Rodrigues Ricky Dinesh <maximusimperoiousthe723rd@gmail.com>
2024-06-29 14:19:44 +0530

    changed acc to reflect my github acc
    there was a mis configuration of my mail it was being sent to a diff acc

--- mill.js
+++ mill.js
@@ -1,4 +1,3 @@
-
 const width = 125;
 const height = 125;
 
@@ -10,38 +9,55 @@ const t = new bt.Turtle();
 const rr = bt.randInRange;
 const sunSize = rr(15, 25);
 
-
-
-const squareSize = 20; // Size of the square
+const squareSize = 20; // Size of the big square
 const centerX = width / 2 + 10;
 const centerY = height / 2 - 20;
 const halfSize = squareSize / 2;
 
-// Function to draw and fill a square
+// Size of the small rectangle
+const smallRectWidth = 10;
+const smallRectHeight = 5;
+
+// Function to draw a rectangle
+function drawRectangle(t, centerX, centerY, halfWidth, halfHeight) {
+  t.up();
+  t.goTo([centerX - halfWidth, centerY - halfHeight]);
+  t.down();
+
+  // Draw the rectangle
+  t.goTo([centerX + halfWidth, centerY - halfHeight]);
+  t.goTo([centerX + halfWidth, centerY + halfHeight]);
+  t.goTo([centerX - halfWidth, centerY + halfHeight]);
+  t.goTo([centerX - halfWidth, centerY - halfHeight]);
+}
+
+// Function to draw and fill the square (modified)
 function drawSquare(t, centerX, centerY, halfSize) {
   t.up();
   t.goTo([centerX - halfSize, centerY - halfSize]);
   t.down();
-  
+
   // Draw the square
   t.goTo([centerX + halfSize, centerY - halfSize]);
   t.goTo([centerX + halfSize, centerY + halfSize]);
   t.goTo([centerX - halfSize, centerY + halfSize]);
   t.goTo([centerX - halfSize, centerY - halfSize]);
+
+  const smallRectCenterX = centerX;
+  const smallRectCenterY = centerY;
+  const smallRectHalfWidth = smallRectWidth / 2;
+  const smallRectHalfHeight = smallRectHeight / 2;
+
   
-  // Filling the square by drawing lines inside it
-  for (let i = 1; i < squareSize; i++) {
-    t.goTo([centerX - halfSize, centerY - halfSize + i]);
-    t.goTo([centerX + halfSize, centerY - halfSize + i]);
-  }
+
+
+  drawRectangle(t, smallRectCenterX, smallRectCenterY, smallRectHalfWidth, smallRectHalfHeight);
 }
 
-// Calling the function to draw and fill the square
+
 drawSquare(t, centerX, centerY, halfSize);
 
 
 bt.join(finalLines, t.lines());
-
-
 
 drawLines(finalLines);
