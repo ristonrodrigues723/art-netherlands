a3c006fab35c09039a716b0daaa174909fc44a7c
Rodrigues Ricky Dinesh <maximusimperoiousthe723rd@gmail.com>
2024-06-30 11:39:09 +0530

    Update flower.js
    chose a simpe kite , gotta creduce size and increase no of times its drawn

--- flower.js
+++ flower.js
@@ -1,4 +1,22 @@
+const t = new bt.Turtle();
 const width = 125;
 const height = 125;
 
 setDocDimensions(width, height);
+
+function drawSimpleKite(t, centerX, centerY, size) {
+  const halfWidth = size / 2;
+  const halfHeight = size * 0.75;
+
+  t.up();
+  t.goTo([centerX, centerY + halfHeight]);
+  t.down();
+  t.goTo([centerX - halfWidth, centerY]);
+  t.goTo([centerX, centerY - halfHeight]);
+  t.goTo([centerX + halfWidth, centerY]);
+  t.goTo([centerX, centerY + halfHeight]);
+}
+
+drawSimpleKite(t, width / 2, height / 2, 40);
+
+drawLines(t.lines());
